beaf460e3230bae07c6b19c96bbadb0db0118607
Henrique Lira <henriquelira.sp@gmail.com>
2025-10-07 19:01:23 -0300

    mudança no layout, implementação de cabeçalho e sessão Entrar em Contato

---
+++ script.js
@@ -0,0 +1,12 @@
+let typed = new Typed("#typed", {
+    strings: [
+        "Desenvolvedor front-end, estudante e apaixonado por tecnologia, ciência e aprendizado.",
+
+    ],
+
+    typeSpeed: 50,
+    backSpeed: 0,
+    smartBackspace: false,
+    loop: false,
+    showCursor: false
+})
